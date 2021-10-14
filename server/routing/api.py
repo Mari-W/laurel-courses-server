0ba1c82058ff1cf640ca2369a83e12b483181ebd
Marius Weidner <weidner@cs.uni-freiburg.de>
2021-10-14 21:19:54 +0200

    rm matplotlib

--- server/routing/api.py
+++ server/routing/api.py
@@ -1,10 +1,10 @@
-from io import BytesIO
+# from io import BytesIO
 
-from flask import Blueprint, jsonify, request, Response
+from flask import Blueprint, jsonify, request
 
 from server.exercises.course import Course
 from server.integration.auth_server import auth
-import matplotlib.pyplot as mpl
+# import matplotlib.pyplot as mpl
 
 from server.routing.decorators import admin_route
 
@@ -39,57 +39,57 @@ def is_tutor(course, tutor):
     return "", 200
 
 
-@api_bp.route("/course/<course>/exercise/<exercise>/points/graph", methods=["GET"])
-@admin_route
-def points(course, exercise):
-    course = Course.from_str(course)
-    if not course:
-        return "course not found", 404
-    exercise = course.get_exercise(exercise)
-    if not exercise:
-        return "exercise not found", 404
-
-    mpl.plot(
-        list(range(round(exercise.points) + 1)),
-        [course.get_students_with_points(points, exercise.name) for points in range(round(exercise.points) + 1)],
-    )
-    mpl.xlabel("Points")
-    mpl.ylabel("Students")
-    mpl.title(f"Points per Students in {exercise.name}")
-    mpl.gcf().gca().yaxis.get_major_locator().set_params(integer=True)
-    mpl.gcf().gca().xaxis.get_major_locator().set_params(integer=True)
-
-    buf = BytesIO()
-    mpl.savefig(buf, format="png")
-    return Response(buf.getvalue(), mimetype='image/png')
-
-
-@api_bp.route("/course/<course>/exercise/<exercise>/time/graph", methods=["GET"])
-@admin_route
-def time(course, exercise):
-    course = Course.from_str(course)
-    if not course:
-        return "course not found", 404
-    exercise = course.get_exercise(exercise)
-    if not exercise:
-        return "exercise not found", 404
-
-    time_spent = course.get_time_spent(exercise.name)
-    max_spent = max(time_spent.keys()) if len(time_spent.keys()) > 0 else 0
-
-    mpl.plot(
-        list(range(max_spent + 1)),
-        [time_spent[spent] if spent in time_spent else 0 for spent in list(range(max_spent + 1))],
-    )
-    mpl.xlabel("Time Spent (in hours, rounded)")
-    mpl.ylabel("Students")
-    mpl.title("Time Spent per Students")
-    mpl.gcf().gca().yaxis.get_major_locator().set_params(integer=True)
-    mpl.gcf().gca().xaxis.get_major_locator().set_params(integer=True)
-
-    buf = BytesIO()
-    mpl.savefig(buf, format="png")
-    return Response(buf.getvalue(), mimetype='image/png')
+# @api_bp.route("/course/<course>/exercise/<exercise>/points/graph", methods=["GET"])
+# @admin_route
+# def points(course, exercise):
+#     course = Course.from_str(course)
+#     if not course:
+#         return "course not found", 404
+#     exercise = course.get_exercise(exercise)
+#     if not exercise:
+#         return "exercise not found", 404
+#
+#     mpl.plot(
+#         list(range(round(exercise.points) + 1)),
+#         [course.get_students_with_points(points, exercise.name) for points in range(round(exercise.points) + 1)],
+#     )
+#     mpl.xlabel("Points")
+#     mpl.ylabel("Students")
+#     mpl.title(f"Points per Students in {exercise.name}")
+#     mpl.gcf().gca().yaxis.get_major_locator().set_params(integer=True)
+#     mpl.gcf().gca().xaxis.get_major_locator().set_params(integer=True)
+#
+#     buf = BytesIO()
+#     mpl.savefig(buf, format="png")
+#     return Response(buf.getvalue(), mimetype='image/png')
+#
+#
+# @api_bp.route("/course/<course>/exercise/<exercise>/time/graph", methods=["GET"])
+# @admin_route
+# def time(course, exercise):
+#     course = Course.from_str(course)
+#     if not course:
+#         return "course not found", 404
+#     exercise = course.get_exercise(exercise)
+#     if not exercise:
+#         return "exercise not found", 404
+#
+#     time_spent = course.get_time_spent(exercise.name)
+#     max_spent = max(time_spent.keys()) if len(time_spent.keys()) > 0 else 0
+#
+#     mpl.plot(
+#         list(range(max_spent + 1)),
+#         [time_spent[spent] if spent in time_spent else 0 for spent in list(range(max_spent + 1))],
+#     )
+#     mpl.xlabel("Time Spent (in hours, rounded)")
+#     mpl.ylabel("Students")
+#     mpl.title("Time Spent per Students")
+#     mpl.gcf().gca().yaxis.get_major_locator().set_params(integer=True)
+#     mpl.gcf().gca().xaxis.get_major_locator().set_params(integer=True)
+#
+#     buf = BytesIO()
+#     mpl.savefig(buf, format="png")
+#     return Response(buf.getvalue(), mimetype='image/png')
 
 
 @api_bp.route("/course/<course>/exercises/stats", methods=["GET"])
